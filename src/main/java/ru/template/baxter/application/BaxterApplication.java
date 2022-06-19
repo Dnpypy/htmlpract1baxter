515ee2141f75e3bb9bce32a13e1094a8b284393a
Dnpypy <denlatypoff@yandex.ru>
2022-06-19 19:31:20 +0300

    Add main class SpringBootApplication

---
+++ src/main/java/ru/template/baxter/application/BaxterApplication.java
@@ -0,0 +1,12 @@
+package ru.template.baxter.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class BaxterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaxterApplication.class, args);
+    }
+}
