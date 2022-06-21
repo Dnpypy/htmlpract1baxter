98fe768e8a175175f297bf51ad422977f82406a3
Dnpypy <denlatypoff@yandex.ru>
2022-06-21 11:55:41 +0300

    Add WebConfiguration class for automatically loading the contents of the validation.properties file into memory

--- src/main/java/ru/template/baxter/application/config/WebSecurityConfig.java
+++ src/main/java/ru/template/baxter/application/config/WebSecurityConfig.java
@@ -1,21 +1,24 @@
 package ru.template.baxter.application.config;
 
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebSecurityConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
 
-    protected void configure(HttpSecurity http) throws Exception{
-        //
+
+    protected void configure(HttpSecurity http) throws Exception {
+        // Empty code!
     }
 }
